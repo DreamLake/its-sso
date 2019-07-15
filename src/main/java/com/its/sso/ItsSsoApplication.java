4e35d6c70323e27549b44ada840aa8ac7806494a
DreamLake <193589685@qq.com>
2019-07-15 17:49:01 +0800

    first commit

---
+++ src/main/java/com/its/sso/ItsSsoApplication.java
@@ -0,0 +1,13 @@
+package com.its.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsSsoApplication.class, args);
+    }
+
+}
